9a4662f431f895a84abfb6424f903e65cde49ae8
AlexFrolov <113615299+ThatGull@users.noreply.github.com>
2023-04-15 18:09:42 +0300

    Add files via upload

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,92 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { queryPixabayAPI } from './servises/PixabayAPI';
-import GlobalStyles from './theme/GlobalStyles';
-import SearchBar from './Searchbar/SearchBar';
-import Button from './Button/Button';
-import ImageGallery from './ImageGallery/ImageGallery';
-import { ThreeDots } from 'react-loader-spinner';
-import { nanoid } from 'nanoid';
+import { useState } from 'react';
+import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
+import { Statistics } from './Statistics/Statistics';
+import { Section } from './Section/Section';
+import { Notification } from './Notification/Notification';
+import PropTypes from 'prop-types';
 
-const App = () => {
-  const [query, setQuery] = useState('');
-  const [page, setPage] = useState(1);
-  const [searchResult, setSearchResult] = useState([]);
-  const [total, setTotal] = useState(0);
-  const [isLoading, setIsLoading] = useState(false);
-  const [id, setId] = useState('');
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const handleSubmit = inputValue => {
-    setQuery(inputValue);
-    setPage(1);
-    setSearchResult([]);
-    const id = nanoid();
-    setId(id);
-    console.log(id);
-  };
-
-  useEffect(() => {
-    if (!query) {
-      return;
+  const updateState = feedback => {
+    switch (feedback) {
+      case 'good':
+        return setGood(good + 1);
+      case 'neutral':
+        return setNeutral(neutral + 1);
+      case 'bad':
+        return setBad(bad + 1);
+      default:
+        return;
     }
-    const fetchImages = async (query, page) => {
-      try {
-        setIsLoading(true);
-        const result = await queryPixabayAPI(query, page);
-
-        if (result.hits.length === 0) {
-          toast.error('No images found');
-        }
-
-        if (page === 1 && result.hits.length !== 0) {
-          toast.success(`${result.total} images found`);
-        }
-        setSearchResult(prevResult => [...prevResult, ...result.hits]);
-        setTotal(result.total);
-      } catch (error) {
-        toast.error('No images found');
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    fetchImages(query, page);
-  }, [query, page, id]);
-
-  const handleLoadMore = () => {
-    setPage(prevPage => prevPage + 1);
   };
 
-  const checkPagesSum = page => {
-    const pagesSum = total / 12;
-    return page < pagesSum;
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
+  };
+
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((100 / countTotalFeedback()) * good);
   };
 
   return (
     <>
-      <GlobalStyles />
-      <SearchBar onSubmit={handleSubmit} />
-
-      <ImageGallery searchResult={searchResult} />
-      {isLoading === true && (
-        <ThreeDots
-          height="80"
-          width="80"
-          radius="9"
-          color="blue"
-          ariaLabel="three-dots-loading"
-          wrapperStyle={{}}
-          wrapperClass="loader"
-          visible={true}
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          updateState={updateState}
         />
-      )}
-      {query !== '' &&
-        searchResult.length !== 0 &&
-        isLoading !== true &&
-        checkPagesSum(page) && <Button handleLoadMore={handleLoadMore} />}
-      <ToastContainer />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() !== 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            totalFeedback={countTotalFeedback()}
+            feedbackPercentage={countPositiveFeedbackPercentage()}
+          />
+        ) : (
+          <Notification message="There is no feedback"></Notification>
+        )}
+      </Section>
     </>
   );
 };
 
-export default App;
+App.propTypes = {
+  feedback: PropTypes.string,
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+  total: PropTypes.number,
+};
